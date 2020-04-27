e48b66bf2d36eb29c6f6f73a30d8e5c176f1a5f2
tlaltenango <donutwine@gmail.com>
2020-04-27 16:31:44 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,6 @@ require_relative '../hashketball'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # config here
+
+  #rspec spec/hashketball_spec.rb:50
 end
